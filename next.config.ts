4a8c69a1b9e717a78841568a7a37f3c94f8c7157
Shubham Rawat <shubham84567@gmail.com>
2026-05-01 20:07:25 +0530

    added basePath and assetPrefix config for GitHub Pages subpath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const gitRepo = 'photo-studio';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath:    process.env.NODE_ENV === 'production' ? `/${gitRepo}`  : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${gitRepo}/` : '',
 };
 
 export default nextConfig;
