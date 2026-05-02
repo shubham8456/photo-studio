3c4a854a41983045b9b8fc0a074ed65a8cd04614
Shubham Rawat <shubham84567@gmail.com>
2026-05-02 15:10:10 +0530

    fixed filtering based on image tag

--- app/components/home/HomePage.tsx
+++ app/components/home/HomePage.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from 'react';
+
 import GalleryItem from "./GalleryItem";
 import Header from "../header";
 import Footer from "../footer";
@@ -7,15 +11,28 @@ import { photoDetails } from "../../../data/photo_details";
 const categories = [...new Set(photoDetails.flatMap(photo => photo.tags.map(tag => tag.toUpperCase())))];
 
 export default function HomePage() {
+  const [activeCategory, setActiveCategory] = useState("ALL");
+
+  const filteredPhotos = activeCategory === "ALL" 
+    ? photoDetails
+    : photoDetails.filter(photo => photo.tags.includes(activeCategory.toLowerCase()));
+
   return (
     <div className="min-h-screen">
       <Header />
       <main className="max-w-[1280px] mx-auto px-margin-mobile md:px-margin-desktop py-unit">
 
-        {/* Filter List */}
+        {/* List of Filters */}
         <section className="mt-2 mb-2">
           <div className="flex gap-4 overflow-x-auto hide-scrollbar py-4 px-2 text-sm">
-            <button className="flex-0 px-3 py-1.5 rounded-full bg-slate-800 text-white flex items-center gap-2 transition-all">
+            <button
+              onClick={() => setActiveCategory("ALL")}
+              className={`flex-0 px-3 py-1.5 rounded-full flex items-center gap-2 transition-all ${
+                activeCategory === "ALL" 
+                ? "bg-slate-800 text-white" 
+                : "bg-white/40 text-slate-600"
+              }`}
+            >
               <span className="material-symbols-outlined text-[12px]">
                 filter_list
               </span>
@@ -25,7 +42,12 @@ export default function HomePage() {
               (category) => (
                 <button
                   key={category}
-                  className="flex-0 px-3 py-1.5 rounded-full bg-white/40 backdrop-blur-md border border-white/20 text-slate-600  hover:text-white  dark:hover:bg-slate-800/60 transition-all"
+                  onClick={() => setActiveCategory(category)}
+                  className={`flex-0 px-3 py-1.5 rounded-full backdrop-blur-md border border-white/20 transition-all ${
+                    activeCategory === category 
+                    ? "bg-slate-800 text-white" 
+                    : "bg-white/40 text-slate-600 hover:bg-slate-800/60 hover:text-white"
+                  }`}
                 >
                   {category}
                 </button>
@@ -36,7 +58,7 @@ export default function HomePage() {
 
         {/* Gallery Items */}
         <section className="masonry-grid pb-12">
-          {photoDetails.map((photo) => (
+          {filteredPhotos.map((photo) => (
             <GalleryItem key={photo.id} {...photo} />
           ))}
         </section>
