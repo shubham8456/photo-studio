115d176bc4575d8719f20a0d2b90ad33756916a5
Shubham Rawat <shubham84567@gmail.com>
2026-05-07 11:52:20 +0530

    fix: removed assetPrefix for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd  = process.env.NODE_ENV === 'production';
-const gitRepo = 'photo-studio';
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath:    isProd ? `/${gitRepo}`  : '',
-  assetPrefix: isProd ? `/${gitRepo}/` : '',
   allowedDevOrigins: ['192.168.1.38'], // replace with device-ip in development to avoid funny issues due to HMR
 };
 
