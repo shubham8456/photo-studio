6730a81d76aa0bf458d1757537f08f87f0f87113
Shubham Rawat <shubham84567@gmail.com>
2026-05-07 10:22:46 +0530

    added deploy script

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [new URL('https://lh3.googleusercontent.com/aida-public/**')],
   },
   basePath:    isProd ? `/${gitRepo}`  : '',
   assetPrefix: isProd ? `/${gitRepo}/` : '',
-  // allowedDevOrigins: ['192.168.1.38'], // replace with device-ip in development to avoid funny issues due to HMR
+  allowedDevOrigins: ['192.168.1.38'], // replace with device-ip in development to avoid funny issues due to HMR
 };
 
 export default nextConfig;
